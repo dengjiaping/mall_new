06e90a693f00fac3d1c51e835c708b1c668ec424
刘再君 <371790785@qq.com>
2017-06-21 16:58:48 +0800

    修改下一步按钮的状态是否可点击

--- Mall/app/src/main/java/com/giveu/shoppingmall/index/activity/UITest.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/index/activity/UITest.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 
 import com.giveu.shoppingmall.base.BaseApplication;
 import com.giveu.shoppingmall.base.DebugConfig;
+import com.giveu.shoppingmall.me.view.activity.LoginActivity;
 
 /**
  * Created by 508632 on 2016/12/22.
@@ -16,7 +17,7 @@ public class UITest {
 		if ( !DebugConfig.isDev ){
 			return;
 		}
-		startActivity(WalletActivationActivity.class);
+		startActivity(LoginActivity.class);
 	}
 
 
---
+++ Mall/app/src/main/java/com/giveu/shoppingmall/utils/listener/TextChangeListener.java
@@ -0,0 +1,19 @@
+package com.giveu.shoppingmall.utils.listener;
+
+import android.text.TextWatcher;
+
+/**
+ * Created by 513419 on 2017/6/21.
+ */
+
+public abstract class TextChangeListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+}
