0270d4259e94fc2415a9ae8a73639506b6402bd4
heyingjun <heyingjun@dafycredit.com>
2017-06-26 19:12:34 +0800

    取现首页UI

---
+++ Mall/app/src/main/java/com/giveu/shoppingmall/cash/view/activity/CashTypeActivity.java
@@ -0,0 +1,112 @@
+package com.giveu.shoppingmall.cash.view.activity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.giveu.shoppingmall.R;
+import com.giveu.shoppingmall.base.BaseActivity;
+import com.giveu.shoppingmall.base.lvadapter.LvCommonAdapter;
+import com.giveu.shoppingmall.base.lvadapter.ViewHolder;
+import com.giveu.shoppingmall.utils.StringUtils;
+import com.giveu.shoppingmall.utils.listener.TextChangeListener;
+import com.giveu.shoppingmall.widget.NoScrollGridView;
+import com.lichfaker.scaleview.HorizontalScaleScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 取现类型页
+ * Created by 101900 on 2017/6/26.
+ */
+
+public class CashTypeActivity extends BaseActivity {
+    TextView mTvVerticalScale;
+    HorizontalScaleScrollView scaleScrollView;
+    @BindView(R.id.et_input_amount)
+    EditText etInputAmount;
+    @BindView(R.id.gv_staging_type)
+    NoScrollGridView gvStagingType;
+    private LvCommonAdapter<String> stagingTypeAdapter;
+    int amount;
+
+    @Override
+    public void initView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_cash_type);
+        baseLayout.setTitle("我要取现");
+
+        scaleScrollView = (HorizontalScaleScrollView) findViewById(R.id.horizontalScale);
+
+
+//        findViewById(R.id.horizontalScaleValueBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String valStr = mTvHorizontalScale.getText().toString().trim();
+//                scaleScrollView.setCurScale(Integer.parseInt(valStr));
+//            }
+//        });
+    }
+
+    @Override
+    public void setListener() {
+        super.setListener();
+        scaleScrollView.setOnScrollListener(new HorizontalScaleScrollView.OnScrollListener() {
+            @Override
+            public void onScaleScroll(int scale) {
+                etInputAmount.setText("" + scale * 50);
+            }
+        });
+
+        etInputAmount.addTextChangedListener(new TextChangeListener() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                etInputAmount.setSelection(s.length());
+                if (StringUtils.isNotNull(s.toString())) {
+                    if (Integer.parseInt(s.toString()) == amount) {//这一次输入与上一次相同，不做操作
+                        return;
+                    }
+                    amount = Integer.parseInt(s.toString());
+                    if (amount >= 0 && amount <= 3000 && (amount % 50 == 0)) {
+                        scaleScrollView.setCurScale(amount);
+                    }
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void setData() {
+        final List<String> data = new ArrayList<>();
+        data.add("按日计息");
+        data.add("24期");
+        data.add("18期");
+        data.add("16期");
+        data.add("12期");
+        data.add("8期");
+        data.add("6期");
+        data.add("4期");
+        data.add("3期");
+        stagingTypeAdapter = new LvCommonAdapter<String>(mBaseContext, R.layout.tv_cash_type_item, data) {
+            @Override
+            protected void convert(ViewHolder viewHolder, String item, int position) {
+                TextView tv_staing_type = viewHolder.getView(R.id.tv_staging_type);
+                tv_staing_type.setText(item);
+            }
+        };
+        gvStagingType.setAdapter(stagingTypeAdapter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+}
--- Mall/app/src/main/java/com/giveu/shoppingmall/index/view/activity/UITest.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/index/view/activity/UITest.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 
 import com.giveu.shoppingmall.base.BaseApplication;
 import com.giveu.shoppingmall.base.DebugConfig;
-import com.giveu.shoppingmall.cash.view.activity.AddressManageActivity;
+import com.giveu.shoppingmall.cash.view.activity.CashTypeActivity;
 
 /**
  * Created by 508632 on 2016/12/22.
@@ -17,7 +17,7 @@ public class UITest {
         if (!DebugConfig.isDev) {
             return;
         }
-        startActivity(AddressManageActivity.class);
+        startActivity(CashTypeActivity.class);
     }
 
 
