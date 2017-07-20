4edbf8d2df094b47a76731cd057918687afe232f
heyingjun <heyingjun@dafycredit.com>
2017-07-20 17:07:40 +0800

    激活修复地理位置权限判断

--- Mall/app/src/main/java/com/giveu/shoppingmall/index/view/activity/WalletActivationFirstActivity.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/index/view/activity/WalletActivationFirstActivity.java
@@ -14,7 +14,6 @@ import com.giveu.shoppingmall.R;
 import com.giveu.shoppingmall.base.BaseActivity;
 import com.giveu.shoppingmall.model.ApiImpl;
 import com.giveu.shoppingmall.model.bean.response.WalletQualifiedResponse;
-import com.giveu.shoppingmall.utils.CommonUtils;
 import com.giveu.shoppingmall.utils.StringUtils;
 import com.giveu.shoppingmall.utils.ToastUtils;
 import com.giveu.shoppingmall.utils.listener.TextChangeListener;
@@ -53,7 +52,6 @@ public class WalletActivationFirstActivity extends BaseActivity {
     public void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_wallet_activation_first);
         baseLayout.setTitle("钱包激活");
-        CommonUtils.openSoftKeyBoard(mBaseContext);
     }
 
     @Override
--- Mall/app/src/main/java/com/giveu/shoppingmall/utils/LocationUtils.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/utils/LocationUtils.java
@@ -32,14 +32,14 @@ public class LocationUtils implements AMapLocationListener {
         //设置是否返回地址信息（默认返回地址信息）
         mLocationOption.setNeedAddress(true);
         //设置只定位一次
-        mLocationOption.setOnceLocation(false);
+        mLocationOption.setOnceLocation(true);
         //设置是否强制刷新WIFI，默认为强制刷新
         mLocationOption.setWifiActiveScan(true);
         //设置是否允许模拟位置,默认为false，不允许模拟位置
         mLocationOption.setMockEnable(false);
         mLocationOption.setLocationCacheEnable(false);
         //设置定位间隔,单位毫秒,5分钟定位一次
-        mLocationOption.setInterval(300000);
+     //   mLocationOption.setInterval(300000);
         //给定位客户端对象设置定位参数
         mLocationClient.setLocationOption(mLocationOption);
     }
