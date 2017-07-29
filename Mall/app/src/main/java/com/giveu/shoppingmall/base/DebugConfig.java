df67afaa1c42ce36474b629206d2e290dd8e10ad
郑飞龙 <zhengfeilong@dafycredit.com>
2017-07-29 14:40:35 +0800

    修改协议地址的正式环境地址

--- Mall/app/src/main/java/com/giveu/shoppingmall/base/DebugConfig.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/base/DebugConfig.java
@@ -10,9 +10,9 @@ import com.giveu.shoppingmall.utils.sharePref.DevSettingSharePref;
  */
 public class DebugConfig {
 
-    public static final boolean isTest = true;
+    public static final boolean isTest = false;
     public static final boolean isDev = false;
-    public static final boolean isOnline = false;
+    public static final boolean isOnline = true;
 
 
     public static boolean isDebug;//true=测试，开发环境 .false=正式环境
@@ -46,7 +46,7 @@ public class DebugConfig {
     public static String FEED_BACK_TEST = "http://wx2.dafycredit.cn/wechat-web/";
 
     //h5 baseurl
-    public static String H5_ONLINE = "http://wx.dafycredit.cn/";
+    public static String H5_ONLINE = "http://3c.dafysz.cn/";
     public static String H5_TEST = "http://wx.dafycredit.cn/";
 
 
