60cfc65f25c5a125c548613498954149460fb378
刘再君 <371790785@qq.com>
2017-09-14 11:24:05 +0800

    【版本号】V1.0.6.4 【变更类型】开发 【代码描述】只有一次性商品

--- Mall/app/src/main/java/com/giveu/shoppingmall/index/view/activity/CommodityDetailActivity.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/index/view/activity/CommodityDetailActivity.java
@@ -113,6 +113,8 @@ public class CommodityDetailActivity extends BasePermissionActivity implements I
         vpContent.setScrollDisabled(false);
         vpContent.setAdapter(new CommodityFragmentAdapter(getSupportFragmentManager(), fragmentList, tabTitles));
         ivCollect.setTag(false);//设置tag标识是否已收藏
+        //目前只有一次性产品
+        isCredit = false;
         //是否分期产品，分期产品显示月供
         if (isCredit) {
             llCollect.getLayoutParams().width = DensityUtils.dip2px(57);
