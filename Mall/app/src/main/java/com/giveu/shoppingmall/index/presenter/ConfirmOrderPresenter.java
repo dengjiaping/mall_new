76c3c5075293c3f467c18b4a1ed7bba9ca05ca5f
zhulanting <zhulanting@dafycredit.com>
2017-10-12 14:55:27 +0800

    【版本号】V1.1.1.1 【变更类型】新增特性 【代码描述】1.接口优化

--- Mall/app/src/main/java/com/giveu/shoppingmall/index/presenter/ConfirmOrderPresenter.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/index/presenter/ConfirmOrderPresenter.java
@@ -1,7 +1,16 @@
 package com.giveu.shoppingmall.index.presenter;
 
+import android.app.Activity;
+
+import com.android.volley.mynet.BaseBean;
+import com.android.volley.mynet.BaseRequestAgent;
 import com.giveu.shoppingmall.base.BasePresenter;
 import com.giveu.shoppingmall.index.view.agent.IConfirmOrderView;
+import com.giveu.shoppingmall.model.ApiImpl;
+import com.giveu.shoppingmall.model.bean.response.CreateOrderResponse;
+import com.giveu.shoppingmall.model.bean.response.DownPayMonthPayResponse;
+import com.giveu.shoppingmall.model.bean.response.MonthSupplyResponse;
+import com.giveu.shoppingmall.model.bean.response.SkuInfo;
 
 /**
  * Created by 524202 on 2017/10/11.
@@ -10,5 +19,47 @@ import com.giveu.shoppingmall.index.view.agent.IConfirmOrderView;
 public class ConfirmOrderPresenter extends BasePresenter<IConfirmOrderView> {
     public ConfirmOrderPresenter(IConfirmOrderView view) {
         super(view);
+    }
+
+    public void getAppDownPayAndMonthPay(Activity context, String channel, String idPerson, int downPaymentRate, String skuCode, int quantity, BaseRequestAgent.ResponseListener<DownPayMonthPayResponse> responseListener) {
+        ApiImpl.getAppDownPayAndMonthPay(context, channel, idPerson, downPaymentRate, skuCode, quantity, new BaseRequestAgent.ResponseListener<DownPayMonthPayResponse>() {
+            @Override
+            public void onSuccess(DownPayMonthPayResponse response) {
+                getView().getAppDownPayAndMonthPaySuccess(response);
+            }
+
+            @Override
+            public void onError(BaseBean errorBean) {
+                getView().getAppDownPayAndMonthPayFailed();
+            }
+        });
+    }
+
+    public void getCreateOrderSc(Activity context, String channel, String idPerson, int downPaymentRate, long idProduct, SkuInfo skuInfo, BaseRequestAgent.ResponseListener<CreateOrderResponse> responseListener) {
+        ApiImpl.createOrderSc(context, channel, idPerson, downPaymentRate, idProduct, skuInfo, new BaseRequestAgent.ResponseListener<CreateOrderResponse>() {
+            @Override
+            public void onSuccess(CreateOrderResponse response) {
+                getView().getCreateOrderScSuccess(response);
+            }
+
+            @Override
+            public void onError(BaseBean errorBean) {
+                getView().getCreateOrderScFailed();
+            }
+        });
+    }
+
+    public void getAppMonthlySupply(Activity context, String channel, String idPerson, int downPaymentRate, long idProduct, int insuranceFee, int quantity, String skuCode, BaseRequestAgent.ResponseListener<MonthSupplyResponse> responseListener) {
+        ApiImpl.getAppMonthlySupply(context, channel, idPerson, downPaymentRate, idProduct, insuranceFee, quantity, skuCode, new BaseRequestAgent.ResponseListener<MonthSupplyResponse>() {
+            @Override
+            public void onSuccess(MonthSupplyResponse response) {
+                getView().getAppMonthlySupplySuccess(response);
+            }
+
+            @Override
+            public void onError(BaseBean errorBean) {
+                getView().getAppMonthlySupplyFailed();
+            }
+        });
     }
 }
--- Mall/app/src/main/java/com/giveu/shoppingmall/index/view/agent/IConfirmOrderView.java
+++ Mall/app/src/main/java/com/giveu/shoppingmall/index/view/agent/IConfirmOrderView.java
@@ -1,10 +1,48 @@
 package com.giveu.shoppingmall.index.view.agent;
 
 import com.giveu.shoppingmall.base.IView;
+import com.giveu.shoppingmall.model.bean.response.CreateOrderResponse;
+import com.giveu.shoppingmall.model.bean.response.DownPayMonthPayResponse;
+import com.giveu.shoppingmall.model.bean.response.MonthSupplyResponse;
 
 /**
  * Created by 524202 on 2017/10/11.
  */
 
 public interface IConfirmOrderView extends IView {
+    /**
+     * 查询首付和分期信息成功回调
+     *
+     * @param response
+     */
+    void getAppDownPayAndMonthPaySuccess(DownPayMonthPayResponse response);
+
+    /**
+     * 查询首付和分期信息失败回调
+     */
+    void getAppDownPayAndMonthPayFailed();
+
+    /**
+     * 创建订单成功回调
+     *
+     * @param response
+     */
+    void getCreateOrderScSuccess(CreateOrderResponse response);
+
+    /**
+     * 创建订单失败回调
+     */
+    void getCreateOrderScFailed();
+
+    /**
+     * 查询月供信息成功回调
+     *
+     * @param response
+     */
+    void getAppMonthlySupplySuccess(MonthSupplyResponse response);
+
+    /**
+     * 查询月供信息失败回调
+     */
+    void getAppMonthlySupplyFailed();
 }
